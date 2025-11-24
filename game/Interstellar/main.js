4f1727dd80e06fc644aed93c4bff9c3f7c6bfca1
TGI <3149897291@qq.com>
2025-11-24 11:46:02 +0800

    更新 main.js

--- game/Interstellar/main.js
+++ game/Interstellar/main.js
@@ -1,7 +1,7 @@
 import { Game } from './src/Game.js';
-// 初始化游戏
- document.addEventListener('DOMContentLoaded', () => {
-   const game = new Game();
-   game.init();
-   game.start();
- });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const game = new Game();
+  game.init();
+  game.start();
+});
